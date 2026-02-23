a5ec03d2df8726caeb52dcc088e220d78554c1af
dewankerjarantingkecpunggelan-hue <dewankerjarantingkecpunggelan@gmail.com>
2026-02-23 14:51:55 +0700

    Update share.js

--- api/share.js
+++ api/share.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const { id } = req.query;
 
   const gasURL =
-    "https://script.google.com/macros/s/AKfycbx-e8JzVEnob7b7QXvoOWnuo5oXi2s4m-Qk5xZQQp0OidxFkCbuRXCjDAWHdNom5LhD/exec?id=" + id;
+    "https://script.google.com/macros/s/AKfycby8ooELYz10mWr_oPs4TVT8aiJjaFHOD2gV3ARaT4lzn2pADHKtBVnJg5jnEkp-owPM/exec/exec?id=" + id;
 
   const response = await fetch(gasURL);
   const html = await response.text();
